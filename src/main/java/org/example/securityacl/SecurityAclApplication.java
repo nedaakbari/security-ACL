ef4573d724ead1ba4483138e7312d416bb46b6c2
n.akbari <n.akbari@mohaymen.ir>
2024-01-09 14:14:04 +0330

    init project

---
+++ src/main/java/org/example/securityacl/SecurityAclApplication.java
@@ -0,0 +1,13 @@
+package org.example.securityacl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAclApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityAclApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/securityacl/SecurityAclApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.securityacl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityAclApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
